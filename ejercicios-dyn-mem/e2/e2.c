199f1421fcc331f92e1de4ace440ec53a618137c
znataniel <znataniel@protonmail.com>
2025-07-02 22:20:34 -0300

    ejercicio 2 resuelto

---
+++ ejercicios-dyn-mem/e2/e2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int cantidad_enteros = 0;
+  int *numeros = NULL;
+  int aux = 0;
+
+  printf("Cuantos enteros desea almacenar?\n");
+  scanf("%d", &cantidad_enteros);
+
+  numeros = malloc(sizeof(int) * cantidad_enteros);
+
+  printf("A continuacion ingrese los numeros de a uno\n");
+  for (int i = 0; i < cantidad_enteros; i++) {
+    scanf("%d", numeros + i);
+  }
+
+  printf("\nIngresaste los numeros\n");
+  for (int i = 0; i < cantidad_enteros; i++) {
+    printf("%d ", numeros[i]);
+  }
+  printf("\n");
+
+  // aca arranca ejercicio 2
+  printf("Ingrese 3 enteros mas\n");
+  numeros = realloc(numeros, (cantidad_enteros + 3) * sizeof(int));
+  for (int i = cantidad_enteros; i < cantidad_enteros + 3; i++) {
+    scanf("%d", numeros + i);
+  }
+  printf("\n");
+
+  printf("Al final todo quedo asi\n");
+  for (int i = 0; i < cantidad_enteros + 3; i++) {
+    printf("%d ", numeros[i]);
+  }
+  printf("\n");
+
+  free(numeros);
+
+  printf("Chau\n");
+  return 0;
+}
